9aa5928ac714627a7ed6a1cc558487918d466b6d
warmstone <719732387@qq.com>
2022-11-04 22:02:49 +0800

    工厂模式

---
+++ src/main/java/com/warmstone/design/pattern/factory/Link.java
@@ -0,0 +1,11 @@
+package com.warmstone.design.pattern.factory;
+
+public abstract class Link extends Item {
+
+    protected String url;
+
+    public Link(String caption, String url) {
+        super(caption);
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/warmstone/design/pattern/factory/Main.java
@@ -0,0 +1,21 @@
+package com.warmstone.design.pattern.factory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java Main class.name.of.ConcreteFactory");
+            System.out.println("Example 1: java Main listfactory.ListFactory");
+            System.out.println("Example 2: java Main tablefactory.TableFactory");
+            System.exit(0);
+        }
+
+        Factory factory = Factory.getFactory(args[0]);
+
+        Link people = factory.createLink("人民日报", "http://www.people.com.cn/");
+        Link gmw = factory.createLink("光明日报", "http://www.gmw.cn/");
+
+        factory.createLink("Yahoo!", "http://www.yahoo.com/");
+//        factory.createLink()
+    }
+}
